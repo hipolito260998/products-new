d930215699a636163d0b88031f164385ef830246
Miguel Hipolito <hipolitoalex45@gmail.com>
2025-07-19 17:18:44 -0600

    repair code

--- app/(products-app)/product/[id].tsx
+++ app/(products-app)/product/[id].tsx
@@ -1,26 +1,48 @@
-import { useEffect } from 'react';
-import { Ionicons } from '@expo/vector-icons';
-import {useNavigation } from 'expo-router';
-import {Text, View} from "react-native";
-
+import {useEffect} from 'react';
+import {Ionicons} from '@expo/vector-icons';
+import {useNavigation} from 'expo-router';
+import {KeyboardAvoidingView, Platform, ScrollView, Text} from "react-native";
+import {ThemedView} from "@/presentation/theme/components/ThemedView";
+import ThemedTextInput from "@/presentation/theme/components/ThemedTextInput";
 
 const ProductScreen = () => {
 
-  const navigation = useNavigation();
+    const navigation = useNavigation();
 
+    useEffect(() => {
+        navigation.setOptions({
+            headerRight: () => <Ionicons name="camera-outline" size={25}/>,
+        });
+    }, []);
 
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => <Ionicons name="camera-outline" size={25} />,
-    });
-  }, []);
-
-
-
-  return (
-    <View>
-        <Text>Product Screen</Text>
-    </View>
-  );
+    return (
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        >
+            <ScrollView>
+                <ThemedView style={{marginHorizontal: 10, marginTop: 20}}>
+                    <ThemedTextInput placeholder='Titulo' style={{marginVertical: 5}}/>
+                    <ThemedTextInput placeholder='Slug' style={{marginVertical: 5}}/>
+                    <ThemedTextInput
+                        placeholder='Descripcion'
+                        multiline={true}
+                        style={{marginVertical: 5}}
+                        numberOfLines={5}
+                    />
+                </ThemedView>
+                <ThemedView
+                    style={{
+                        marginHorizontal: 10,
+                        marginVertical: 5,
+                        flexDirection: 'row',
+                        gap: 10
+                    }}
+                >
+                    <ThemedTextInput  placeholder='Precio' style={{flex:1}}/>
+                    <ThemedTextInput placeholder='Inventario' style={{flex:1}} />
+                </ThemedView>
+            </ScrollView>
+        </KeyboardAvoidingView>
+    );
 };
 export default ProductScreen;
--- presentation/theme/components/ThemedTextInput.tsx
+++ presentation/theme/components/ThemedTextInput.tsx
@@ -1,61 +1,63 @@
-import { Ionicons } from '@expo/vector-icons';
-import React, { useRef, useState } from "react";
-import { StyleSheet, TextInput, TextInputProps, View } from "react-native";
-import { useThemeColor } from "../hooks/useThemeColor";
+import {Ionicons} from '@expo/vector-icons';
+import React, {useRef, useState} from "react";
+import {StyleProp, StyleSheet, TextInput, TextInputProps, View, ViewStyle} from "react-native";
+import {useThemeColor} from "../hooks/useThemeColor";
 
 interface Props extends TextInputProps {
-  icon?: keyof typeof Ionicons.glyphMap;
+    icon?: keyof typeof Ionicons.glyphMap
+
 }
 
-const ThemedTextInput = ({ icon, ...rest }: Props) => {
-  const primaryColor = useThemeColor({}, "primary");
-  const textColor = useThemeColor({}, "text");
 
-  const [isActive, setIsActive] = useState(false);
-  const inputRef = useRef<TextInput>(null);
+const ThemedTextInput = ({icon, style, ...rest}: Props) => {
+    const primaryColor = useThemeColor({}, "primary");
+    const textColor = useThemeColor({}, "text");
 
-  return (
-    <View
-      style={{
-        ...styles.border,
-        borderColor: isActive ? primaryColor : "#cccccc",
-      }}
-      onTouchStart={() => inputRef.current?.focus()}
-    >
-      {icon && (
-        <Ionicons
-          name={icon}
-          size={20}
-          color={isActive ? primaryColor : textColor}
-          style={{ marginRight: 10 }}
-        />
-      )}
-      <TextInput
-        ref={inputRef}
-        placeholderTextColor="#5c5c5c"
-        {...rest}
-        onFocus={() => setIsActive(true)}
-        onBlur={() => setIsActive(false)}
-        style={{
-          color: textColor,
-          marginRight: 10,
-          flex: 1,
-        }}
-      />
-      
-    </View>
-  );
+    const [isActive, setIsActive] = useState(false);
+    const inputRef = useRef<TextInput>(null);
+
+    return (
+        <View
+            style={[{
+                ...styles.border,
+                borderColor: isActive ? primaryColor : "#cccccc",
+            },style as StyleProp<ViewStyle>]}
+            onTouchStart={() => inputRef.current?.focus()}
+        >
+            {icon && (
+                <Ionicons
+                    name={icon}
+                    size={20}
+                    color={isActive ? primaryColor : textColor}
+                    style={{marginRight: 10}}
+                />
+            )}
+            <TextInput
+                ref={inputRef}
+                placeholderTextColor="#5c5c5c"
+                {...rest}
+                onFocus={() => setIsActive(true)}
+                onBlur={() => setIsActive(false)}
+                style={[{
+                    color: textColor,
+                    marginRight: 10,
+                    flex: 1,
+                }]}
+            />
+
+        </View>
+    );
 };
 
 export default ThemedTextInput;
 
 const styles = StyleSheet.create({
-  border: {
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 5,
-    marginBottom: 10,
-    flexDirection: "row",
-    alignItems: "center",
-  },
+    border: {
+        borderWidth: 1,
+        borderRadius: 5,
+        padding: 5,
+        marginBottom: 10,
+        flexDirection: "row",
+        alignItems: "center",
+    },
 });
